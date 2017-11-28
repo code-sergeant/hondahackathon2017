3b8075226b11ab19dee6c44c61d5635573a79af8
joe niccoli <niccoli.joseph@gmail.com>
2017-11-28 09:48:18 -0800

    wip

---
+++ net5hondahack.Web/Scripts/app/services/BikerLocationService1.js
@@ -0,0 +1,32 @@
+﻿(function () {
+	'use strict';
+
+	angular.module(APPNAME)
+		.service('BikeLocationService', BikeLocationService);
+	BikeLocationService.$inject = ['$http'];
+
+	function BikeLocationService($http) {
+		var vm = this;
+
+		vm.create = _create;
+		vm.update = _update;
+
+
+		function _update(data) {
+			return $http({
+				method: 'GET',
+				url: '/api/BikeLocation/' + data.id,
+				data: data
+			})
+		}
+
+		function _create(data) {
+			return $http({
+				method: 'PUT',
+				url: '/api/Bikelocation',
+				data: data
+			})
+		}
+	}
+})();
+

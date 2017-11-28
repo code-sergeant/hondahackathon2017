3c3edec471e023cf85b079ef144a4b5d9e12632b
Steven Hernandez <code_sergeant@protonmail.com>
2017-11-28 09:35:26 -0800

    wip

---
+++ Scripts/app/services/bikerLocationService.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+
+    angular.module(APPNAME)
+        .service('BikerLocationFactory', BikerLocationFactory);
+
+    BikerLocationFactory.$inject = ['$q', '$http', 'CycleDataService', 'CarDataService'];
+
+    function BikerLocationFactory($q, $http, CycleDataService, CarDataService) {
+        const svc = this;
+
+        svc.cycleLocations = CycleDataService.cycleLocations;
+        svc.getCycleLocation = _getCycleLocation;
+
+        function _getCycleLocation() {
+            navigator.geolocation.getCurrentPosition(position => {
+                const location = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                };
+                svc.cycleLocations.push(location);
+            });
+        }
+    }
+})();
---
+++ net5hondahack.Web/Scripts/index.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+
+    window.APPNAME = 'SafeLane';
+
+    var app = angular.module(APPNAME, []);
+
+    // add X-Requested-With: XMLHttpRequest to all of our ajax calls (so that the server knows
+    // to not redirect to the login page when unauthorized)
+    app.config(ConfigureHttp);
+    ConfigureHttp.$inject = ['$httpProvider'];
+    function ConfigureHttp($httpProvider) {
+        $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+    }
+
+    // enable HTML5-compatible URLs
+    app.config(ConfigureLocationProvider);
+    ConfigureLocationProvider.$inject = ['$locationProvider'];
+    function ConfigureLocationProvider($locationProvider) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    }
+})();

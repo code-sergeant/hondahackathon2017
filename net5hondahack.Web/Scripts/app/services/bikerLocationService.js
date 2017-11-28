847e330ae0cc72b7c2b0fc06cb7896c48efc123a
Jordan Jesse <jordanljesse@gmail.com>
2017-11-27 19:38:00 -0800

    add getCycleLocation to bikerLocationService.js (#3)

--- net5hondahack.Web/Scripts/app/services/bikerLocationService.js
+++ net5hondahack.Web/Scripts/app/services/bikerLocationService.js
@@ -2,34 +2,27 @@
     'use strict';
 
     angular.module(APPNAME)
-        .factory('BikerLocationFactory', BikerLocationFactory);
+        .service('BikerLocationFactory', BikerLocationFactory);
 
-    BikerLocationFactory.$inject = ['$q', '$http'];
+    BikerLocationFactory.$inject = ['$q', '$http', 'CycleDataService', 'CarDataService'];
 
-    function BikerLocationFactory($q, $http) {
+    function BikerLocationFactory($q, $http, CycleDataService, CarDataService) {
+        const svc = this;
 
-        /////// Available services TOC ///////
-        const service = {
-            getBikerLoc: _getBikerLoc,
-            postBikerLoc: _postBikerLoc,
-            updateBikerLoc: _updateBikerLoc
-        };
 
-        return service;
+        svc.cycleLocations = CycleDataService.cycleLocations;
+        svc.getCycleLocation = _getCycleLocation;
 
-        /////// Implementation details ///////
-        function _getBikerLoc() {
-            $http.post()
-                .then();
-        };
 
-        function _postBikerLoc() {
-            $http.post()
-                .then();
-        };
+        function _getCycleLocation() {
+            navigator.geolocation.getCurrentPosition(position => {
+                const location = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                };
 
-        function _updateBikerLoc() {
-
-        };
+                svc.cycleLocations.push(location);
+            });
+        }
     }
 })();
